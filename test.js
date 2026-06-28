ec0700f5f3a3e1727bb771df181a330602eda2f2
Deepanshu Kumar <deepanshu.kumar@iitg.ac.in>
2026-06-29 01:36:09 +0530

    Add geocoding: Auto-convert addresses to coordinates in backend

--- test.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const { getCoordinates } = require('./utils/geoService');
-
-getCoordinates("Dehradun, India")
-    .then(coords => console.log("Success! Coordinates:", coords))
-    .catch(err => console.error("Error:", err.message));
